7f7f4c26ee373ab329e37416449bdcdb50281da3
inzk113 <l2402810376as@gmail.com>
2024-11-23 10:59:20 +0900

    プルリクエストの復習

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -15,8 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentManagementApplication {
 
-  @Autowired
-  private  StudentRepository repository;
+
 
   private Map<String, String> students = new HashMap<>();
 
