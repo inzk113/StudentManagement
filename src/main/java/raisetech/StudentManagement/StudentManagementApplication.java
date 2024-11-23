3d401153faadb1aa389647b5b12d0124419652e8
inzk113 <l2402810376as@gmail.com>
2024-11-23 10:55:19 +0900

    プルリクエストの復習

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -3,6 +3,7 @@ package raisetech.StudentManagement;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentManagementApplication {
 
+  @Autowired
+  private  StudentRepository repository;
   private Map<String, String> students = new HashMap<>();
 
   public StudentManagementApplication() {
@@ -39,25 +42,3 @@ public class StudentManagementApplication {
 
   }
 }
-//  private String name = "Enami Kouji";
-//  private String age ="37";
-//  public static void main(String[] args) {
-//    SpringApplication.run(StudentManagementApplication.class, args);
-//  }
-
-//  @GetMapping("/studentInfo")
-//  public String getStudentInfo() {
-//    return name + " "+ age + "歳";
-//  }
-//
-//  @PostMapping("/studentInfo")
-//  public void setStudentInfo(String name,String age) {
-//    this.name = name;
-//    this.age =age;
-//  }
-//
-//  @PostMapping("/studentName")
-//  public void updateStudentName(String name){
-//    this.name = name;
-//  }
-//}
