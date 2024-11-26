96cd2329d8cfbff2d658fca69fb358afd295f4b3
inzk113 <l2402810376as@gmail.com>
2024-11-26 20:22:29 +0900

    スタンダード第7回演習課題

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,63 +1,65 @@
 package raisetech.StudentManagement;
 
 
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
-  private Map<String, String> students = new HashMap<>();
-
-  public StudentManagementApplication() {
-    students.put("27", "Mayu Inuzuka");
-    students.put("30", "taro Yamada");
-    students.put("25", "Hanako Sato");
-
-  }
+  //  private Map<String, String> students = new HashMap<>();
+//
+//  public StudentManagementApplication() {
+//    students.put("27", "Mayu Inuzuka");
+//    students.put("30", "taro Yamada");
+//    students.put("25", "Hanako Sato");
+//
+//  }
+//
+//  public static void main(String[] args) {
+//    SpringApplication.run(StudentManagementApplication.class, args);
+//  }
+//
+//  @GetMapping("/students")
+//  public Map<String, String> getStudentInfo() {
+//    return students;
+//  }
+//
+//  @PostMapping("/students")
+//  public String addStudent(@RequestParam String age, @RequestParam String name) {
+//    students.put(age, name);
+//    return "Student added successfully";
+//
+//  }
+//}
+  @Autowired
+  private StudentRepository repository;
+  private String name = "Enami Kouji";
+  private String age = "37";
 
   public static void main(String[] args) {
     SpringApplication.run(StudentManagementApplication.class, args);
   }
 
-  @GetMapping("/students")
-  public Map<String, String> getStudentInfo() {
-    return students;
+  @GetMapping("/studentInfo")
+  public String getStudentInfo() {
+    Student student = repository.searchByName("InuzukaMayu");
+    return student.getName() + " " + student.getAge() + "歳";
   }
 
-  @PostMapping("/students")
-  public String addStudent(@RequestParam String age, @RequestParam String name) {
-    students.put(age, name);
-    return "Student added successfully";
+  @PostMapping("/studentInfo")
+  public void setStudentInfo(String name, String age) {
+    this.name = name;
+    this.age = age;
+  }
 
+  @PostMapping("/studentName")
+  public void updateStudentName(String name) {
+    this.name = name;
   }
 }
-//  private String name = "Enami Kouji";
-//  private String age ="37";
-//  public static void main(String[] args) {
-//    SpringApplication.run(StudentManagementApplication.class, args);
-//  }
-
-//  @GetMapping("/studentInfo")
-//  public String getStudentInfo() {
-//    return name + " "+ age + "歳";
-//  }
-//
-//  @PostMapping("/studentInfo")
-//  public void setStudentInfo(String name,String age) {
-//    this.name = name;
-//    this.age =age;
-//  }
-//
-//  @PostMapping("/studentName")
-//  public void updateStudentName(String name){
-//    this.name = name;
-//  }
-//}
---
+++ src/main/java/raisetech/StudentManagement/StudentRepository.java
@@ -0,0 +1,13 @@
+package raisetech.StudentManagement;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface StudentRepository {
+
+  @Select("SELECT * FROM student WHERE name = #{name}")
+  Student searchByName(String name);
+
+}
+
