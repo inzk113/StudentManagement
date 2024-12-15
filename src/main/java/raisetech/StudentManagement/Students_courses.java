4c2cc01303358bcfc70eeb3a626e671a06d1abe9
inzk113 <l2402810376as@gmail.com>
2024-12-15 15:41:11 +0900

    スタンダード第10回演習課題

---
+++ src/main/java/raisetech/StudentManagement/Students_courses.java
@@ -0,0 +1,17 @@
+package raisetech.StudentManagement;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class Students_courses {
+
+  private String id;
+  private String student_id;
+  private String course_name;
+  private String start_date;
+  private String end_date;
+}
+
