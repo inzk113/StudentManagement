8021ad461883fb4ef0af6bccd4501770dd3a23a3
inzk113 <l2402810376as@gmail.com>
2025-01-26 12:07:37 +0900

    スタンダード第16回演習課題

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 
@@ -22,18 +23,52 @@ public interface StudentRepository {
    */
 
   @Select("SELECT * FROM students")
-  List<Student> searchStudent();
+  List<Student> search();
 
+  //search()
+  @Select("SELECT * FROM students WHERE id = #{id}")
+  Student searchstudent(String id);
+
+  //serchstudent()
   @Select("SELECT * FROM students_courses")
-  List<StudentsCourses> searchStudentsCourses();
+  List<StudentsCourses> searchStudentsCoursesList();
 
-  @Insert("INSERT INTO students (id,name, age,city,email, nickName,gender,remark,isDeleted)"
-      + " VALUES (#{id},#{name}, #{age},#{city}, #{email}, #{nickName},#{gender},#{remark},false)")
-  @Options(useGeneratedKeys = true, keyProperty = "studentId")
+  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
+  List<StudentsCourses> searchStudentsCourses(String studentId);
+
+
+  //IDは自動採番にするからいらない
+  @Insert("INSERT INTO students (name, age,city,email, nick_name,gender,remark,is_deleted) "
+      + " VALUES (#{name}, #{age},#{city}, #{email}, #{nickName},#{gender},#{remark},false)")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
   void registerStudent(Student student);
 
+
+  @Insert("INSERT INTO students_courses(studentId,courseName,startDate,endDate) "
+      + "VALUES(#{studentId},#{courseName},#{startDate},#{endDate})")
+
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void registerStudentsCourses(StudentsCourses studentsCourses);
+
+
+  @Update(
+      "UPDATE students SET name = #{name}, nick_name = #{nickName}, email = #{email}, city = #{city}, "
+          + "age = #{age}, gender = #{gender}, remark = #{remark} WHERE id = #{id}")
+  void updateStudent(Student student);
+
+
+  @Update("UPDATE students_courses SET course_name = #{courseName} WHERE id = #{id} ")
+  void updateStudentCourses(StudentsCourses studentsCourses);
 }
 
+//受講生更新のHTMLを作成しましょう
+//（受講生登録を参考にしましょう）
+//registerのコピペでいい　
+//studentListの名前のところに「エータグ」をやるとリンクができる
+//@GetMapping("/updateStudent)
+
+//受講生一覧の表で名前をクリックすると、その受講生のID情報に
+//もとづいたデータを表示できるようにしましょう
 
 
 
--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -1,18 +1,19 @@
 package raisetech.StudentManagement.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
-import raisetech.StudentManagement.domein.StudentDetail;
+import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.repository.StudentRepository;
 
 @Service
 public class StudentService {
 
-  //  private StudentRepository repository;
+
   private StudentRepository repository;
 
   @Autowired
@@ -20,16 +21,49 @@ public class StudentService {
     this.repository = repository;
   }
 
-  public List<Student> searchStudentList() {
-    return repository.searchStudent();
+//  public List<Student> serchStudentList() {
+//    return repository.search();
+//  }
+
+
+
+  public StudentDetail findStudentById(String id) {
+    Student student = repository.searchstudent(id);
+    List<StudentsCourses> studentsCourses = repository.searchStudentsCourses(student.getId());
+
+    StudentDetail studentDetail = new StudentDetail();
+    studentDetail.setStudent(student);
+    studentDetail.setStudentsCourses(studentsCourses);
+    return studentDetail;
   }
 
-  public List<StudentsCourses> searchStudentsCoursesList() {
-    return repository.searchStudentsCourses();
+  public List<Student> serchStudentList() {
+    return repository.search();
+  }
+
+  public List<StudentsCourses> searchStudentsCourseList() {
+    return repository.searchStudentsCoursesList();
   }
 
   @Transactional
   public void registerStudent(StudentDetail studentDetail) {
     repository.registerStudent(studentDetail.getStudent());
+
+    for (StudentsCourses studentsCourse : studentDetail.getStudentsCourses()) {
+
+      studentsCourse.setStudentId(studentDetail.getStudent().getId());
+      studentsCourse.setStartDate(java.sql.Timestamp.valueOf(LocalDateTime.now()));
+      studentsCourse.setEndDate(java.sql.Timestamp.valueOf(LocalDateTime.now().plusYears(1)));
+
+      repository.registerStudentsCourses(studentsCourse);
+    }
+  }
+
+  @Transactional
+  public void updateStudent(StudentDetail studentDetail) {
+    repository.updateStudent(studentDetail.getStudent());
+    for (StudentsCourses studentsCourse : studentDetail.getStudentsCourses()) {
+      repository.updateStudentCourses(studentsCourse);
+    }
   }
 }
