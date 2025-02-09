d2f4961a31aae89b8cbb597e209e3c84a06ad58a
inzk113 <l2402810376as@gmail.com>
2025-02-09 11:50:54 +0900

    スタンダード第22回演習課題

--- src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
+++ src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import raisetech.StudentManagement.data.Student;
-import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.data.StudentCourse;
 
 @Getter
 @Setter
@@ -16,7 +16,7 @@ public class StudentDetail {
 
 
   private Student student;
-  private List<StudentsCourses> studentsCourses;
+  private List<StudentCourse> studentCourseList;
 
 }
 
--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 import raisetech.StudentManagement.data.Student;
-import raisetech.StudentManagement.data.StudentsCourses;
+import raisetech.StudentManagement.data.StudentCourse;
 
 
 /**
@@ -39,7 +39,7 @@ public interface StudentRepository {
    * @return 受講生のコース情報（全件）
    */
   @Select("SELECT * FROM students_courses")
-  List<StudentsCourses> searchStudentsCoursesList();
+  List<StudentCourse> searchStudentCoursesList();
 
   /**
    * 受講生IDに紐づく受講生コース情報を検索します。
@@ -48,40 +48,47 @@ public interface StudentRepository {
    * @return　受講生IDに紐づく受講生コース情報
    */
   @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
-  List<StudentsCourses> searchStudentsCourses(String studentId);
+  List<StudentCourse> searchStudentCourse(String studentId);
 
-
-  //IDは自動採番にするからいらない
+  /**
+   * 受講生を新規登録します。
+   * IDに関しては自動採番を行う。
+   *
+   * @param student　受講生
+   */
   @Insert("INSERT INTO students (name, age,city,email, nick_name,gender,remark,is_deleted) "
       + " VALUES (#{name}, #{age},#{city}, #{email}, #{nickName},#{gender},#{remark},false)")
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void registerStudent(Student student);
 
-
+  /**
+   * 受講生コース情報を新規登録します。
+   * IDに関しては自動採番を行う。
+   *
+   * @param studentCourse　受講生コース情報
+   */
   @Insert("INSERT INTO students_courses(student_id,course_name,start_date,end_date) "
       + "VALUES(#{studentId},#{courseName},#{startDate},#{endDate})")
 
   @Options(useGeneratedKeys = true, keyProperty = "id")
-  void registerStudentsCourses(StudentsCourses studentsCourses);
+  void registerStudentCourse(StudentCourse studentCourse);
 
-
+  /**
+   * 受講生を更新します。
+   *
+   * @param student　受講生
+   */
   @Update(
       "UPDATE students SET name = #{name}, nick_name = #{nickName}, email = #{email}, city = #{city}, "
           + "age = #{age}, gender = #{gender}, remark = #{remark},is_deleted=#{isDeleted} WHERE id = #{id}")
   void updateStudent(Student student);
 
-
+  /**
+   * 受講生コース情報のコース名を更新します。
+   * @param studentCourse　受講生コース情報
+   */
   @Update("UPDATE students_courses SET course_name = #{courseName} WHERE id = #{id} ")
-  void updateStudentCourses(StudentsCourses studentsCourses);
+  void updateStudentCourse(StudentCourse studentCourse);
 }
-
-//受講生更新のHTMLを作成しましょう
-//（受講生登録を参考にしましょう）
-//registerのコピペでいい　
-//studentListの名前のところに「エータグ」をやるとリンクができる
-//@GetMapping("/updateStudent)
-
-
-
 
 
